adda82a8c21079f8f40b767baf7d527231e0a21f
chamedm <is708177@iteso.mx>
2021-04-27 14:23:39 -0500

    Adding alert+state management

--- src/app/App.js
+++ src/app/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {BrowserRouter as Router, Route, } from 'react-router-dom';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import SignIn from '../identity/SignIn';
 import Dashboard from './Dashboard';
 
@@ -8,10 +8,12 @@ function App() {
 
   return (
     <Router>
-      <Route path='/signIn' component={SignIn}/> 
+      <Route path='/' exact component={SignIn}/> 
       <Route path='/dashboard' component={Dashboard}/>
+      
     </Router>
   );
 }
+
 
 export default App;
--- src/card/PlaceItem.js
+++ src/card/PlaceItem.js
@@ -4,10 +4,10 @@ import InfoCard from '../card/InfoCard';
 import './PlaceItem.style.css';
 
 
-function PlaceItem({data}) {
+function PlaceItem({data, openDialog}) {
   return(
     <div className="place-item">
-      <InfoCard data={data}/>
+      <InfoCard data={data} openDialog={openDialog}/>
       <LineChart data={data.capacityLog}/>
     </div>
   )
